1161ac652da1cffe2eea7c5b5fb849ea3b2e807a
syjn99 <syjn99@gmail.com>
2022-05-08 23:08:05 +0900

    update package.json

---
+++ build/precache-manifest.e1e564e4c44bbf13b9d4b8d9ce639d4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa75cf0534c9f8f054d3cf593bf47797",
+    "url": "/honk-portal-frontend/index.html"
+  },
+  {
+    "revision": "230ef91c6a0e76d1938b",
+    "url": "/honk-portal-frontend/static/css/main.edb83f00.chunk.css"
+  },
+  {
+    "revision": "843f37afb5dccf45bef1",
+    "url": "/honk-portal-frontend/static/js/2.2f637533.chunk.js"
+  },
+  {
+    "revision": "230ef91c6a0e76d1938b",
+    "url": "/honk-portal-frontend/static/js/main.1b10c31d.chunk.js"
+  },
+  {
+    "revision": "4ec4a7729e63cb30e957",
+    "url": "/honk-portal-frontend/static/js/runtime-main.d4752b95.js"
+  },
+  {
+    "revision": "1bc6ff0a00a37b9c0569f8e0db1cbd56",
+    "url": "/honk-portal-frontend/static/media/profile.1bc6ff0a.png"
+  }
+]);
